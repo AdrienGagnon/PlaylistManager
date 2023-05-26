f31e7b36f82779e11ff48356a0ecb2056ba5da63
Adrien Gagnon <118489764+AdrienGagnon@users.noreply.github.com>
2023-05-26 18:39:12 -0400

    change callback

--- src/authentication/Callback.jsx
+++ src/authentication/Callback.jsx
@@ -1,10 +1,13 @@
 import setToken from './setToken';
 
 import styles from './Callback.module.css';
+import { useEffect } from 'react';
 
 function CallBack() {
     setToken();
-
+    useEffect(() => {
+        // window.location.href = '/';
+    }, []);
     return <div className={styles.container}>Loading content...</div>;
 }
 
