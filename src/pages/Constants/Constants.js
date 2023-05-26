615c2dfdb6188d3980354fa7f6aa27ade7218ef9
Adrien Gagnon <118489764+AdrienGagnon@users.noreply.github.com>
2023-05-26 18:19:43 -0400

    fix constants js

--- src/pages/Constants/Constants.js
+++ src/pages/Constants/Constants.js
@@ -1,13 +1,13 @@
-ClientID = '315507717e724bc69c8557d906eaf6a8';
-REDIRECTURI = 'http://localhost:1234/';
-ClientSecret = '89d6adea905c438ab928dc14a6e06b44';
+const ClientID = '315507717e724bc69c8557d906eaf6a8';
+const REDIRECTURI = 'http://localhost:1234/';
+const ClientSecret = '89d6adea905c438ab928dc14a6e06b44';
 
-SPOTIFY_GREEN = '30,215,96';
-SPOTIFY_BLACK = '25,20,20';
-SPOTIFY_DARK_GREY = '33,33,33';
-SPOTIFY_GREY = '83,83,83';
-SPOTIFY_LIGHT_GREY = '179,179,179';
-SPOTIFY_WHITE = '255,255,255';
+const SPOTIFY_GREEN = '30,215,96';
+const SPOTIFY_BLACK = '25,20,20';
+const SPOTIFY_DARK_GREY = '33,33,33';
+const SPOTIFY_GREY = '83,83,83';
+const SPOTIFY_LIGHT_GREY = '179,179,179';
+const SPOTIFY_WHITE = '255,255,255';
 
 export { ClientID, REDIRECTURI, ClientSecret };
 export { SPOTIFY_GREEN };
