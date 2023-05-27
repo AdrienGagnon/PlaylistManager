b9d432966e7161bda71229d714024aee3e986fcb
Adrien Gagnon <118489764+AdrienGagnon@users.noreply.github.com>
2023-05-26 20:08:38 -0400

    fix timeoute

--- src/authentication/Callback.jsx
+++ src/authentication/Callback.jsx
@@ -1,17 +1,10 @@
 import setToken from './setToken';
 
 import styles from './Callback.module.css';
-import { useEffect } from 'react';
 
 function CallBack() {
     setToken();
-    useEffect(() => {
-        // const timeoutID = setTimeout(() => {
-        //     window.location.href =
-        //         'https://playlist-manager-ag.netlify.app/Home';
-        // }, 5000);
-        return clearTimeout(timeoutID);
-    }, []);
+
     return <div className={styles.container}>Loading content...</div>;
 }
 
